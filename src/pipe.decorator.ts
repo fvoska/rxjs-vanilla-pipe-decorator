7e45ca99faf8e28383ec4ea327bf15fe7b561bae
Filip Voska <filip.voska@infinum.hr>
2019-11-28 10:29:57 +0100

    Step 5

--- src/pipe.decorator.ts
+++ src/pipe.decorator.ts
@@ -1,11 +1,23 @@
+import { Subject } from 'rxjs';
+
 export function Pipe(
-  operators: Array<Function>
+  operators: Array<Function>,
 ) {
   return function (
     target: any, // Object's prototype
     propertyKey: string, // Key of the member that we are decorating
     descriptor: PropertyDescriptor, // Property descriptor
   ) {
-    console.log(target, propertyKey, descriptor);
+    const source$ = new Subject<IArguments>();
+
+    const originalMethod: Function = descriptor.value;
+
+    source$.subscribe((args: IArguments) => {
+      originalMethod(...args);
+    });
+
+    descriptor.value = function() {
+      source$.next(arguments);
+    }
   }
 }
