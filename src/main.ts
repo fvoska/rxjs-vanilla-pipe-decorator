6ca992e4a64e7f9eb67f21a1e4a0960f749b0bf2
Filip Voska <filip.voska@infinum.hr>
2019-11-28 10:11:52 +0100

    Step 3

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,20 @@
-import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { Pipe } from './pipe.decorator';
 
 class Component {
-  private click$ = new Subject<MouseEvent>();
-
   constructor() {
     const button: HTMLButtonElement = document.querySelector('.click-me');
 
     button.addEventListener('click', (event) => {
       this.handleClick(event);
     });
-
-    this.click$.pipe(
-      debounceTime(250),
-    ).subscribe((event) => {
-      console.log(event);
-    });
   }
 
+  @Pipe([
+    debounceTime(250),
+  ])
   private handleClick(event: MouseEvent): void {
-    this.click$.next(event);
+    console.log(event);
   }
 }
 
