6155c0ac834423e4e02328d1024c8030eca2fc47
Filip Voska <filip.voska@infinum.hr>
2019-11-28 09:59:37 +0100

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
+import { Subject } from 'rxjs';
+
 class Component {
+  private click$ = new Subject<MouseEvent>();
+
   constructor() {
     const button: HTMLButtonElement = document.querySelector('.click-me');
 
     button.addEventListener('click', (event) => {
       this.handleClick(event);
     });
+
+    this.click$.subscribe((event) => {
+      console.log(event);
+    });
   }
 
   private handleClick(event: MouseEvent): void {
-    console.log(event);
+    this.click$.next(event);
   }
 }
 
